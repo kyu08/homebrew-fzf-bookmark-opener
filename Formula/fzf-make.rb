6fef757538f5178019ff4cfd02a229ef65bd5939
kyu08 <49891479+kyu08@users.noreply.github.com>
2023-03-31 00:56:06 +0900

    update fzf-make to v0.4.0

--- Formula/fzf-make.rb
+++ Formula/fzf-make.rb
@@ -1,8 +1,8 @@
 class FzfMake < Formula
   desc "The command line tool that execute make command using fzf."
   homepage ""
-  url "https://github.com/kyu08/fzf-make/releases/download/v0.3.0/fzf-make"
-  sha256 "b53afee991696b7d6458f9c9775f08ce3a9220baaac0d39d0d64c11c1bc32683"
+  url "https://github.com/kyu08/fzf-make/releases/download/v0.4.0/fzf-make"
+  sha256 "f3cb20e5281df55fb39e3d1625f272ce836966656754409c4f22a0b80207b59e"
   license "MIT"
   depends_on "bat"
 
