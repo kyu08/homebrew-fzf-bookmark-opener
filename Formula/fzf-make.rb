b4a333395918cc1bc368ca4536bcd200b22528bf
kyu08 <49891479+kyu08@users.noreply.github.com>
2023-10-08 02:02:07 +0900

    [test][fzf-make] cargo install

--- Formula/fzf-make.rb
+++ Formula/fzf-make.rb
@@ -1,13 +1,14 @@
 class FzfMake < Formula
   desc "A command line tool that executes make target using fuzzy finder with preview window."
   homepage "https://github.com/kyu08/fzf-make"
-  url "https://github.com/kyu08/fzf-make/releases/download/v0.7.0/fzf-make"
+  url "https://github.com/kyu08/fzf-make/archive/refs/tags/v0.7.0.tar.gz"
   sha256 "b99d1028669a2993c97d049a7925afa23aea213b292c5fd21e12e19866eeb69a"
   license "MIT"
   depends_on "bat"
+  depends_on "rust" => :build
 
   def install
-    bin.install "fzf-make"
+    system "cargo", "install", "--root", prefix, "--path", "."
 
     message = <<-EOS
 ===============================================================================================
@@ -42,4 +43,9 @@ https://github.com/kyu08/fzf-make/issues
 
     puts(message)
   end
+
+  test do
+    assert_match(/^v.*$/, pipe_output("#{bin}/fzf-make --version"))
+  end
+
 end
